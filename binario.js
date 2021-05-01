2a5d789571ae485caea3f03fda65fba0d825802a
sebas <juansebas08004@gmail.com>
2021-04-30 21:31:38 -0500

    mi fallo intentando hacer una cosa

--- binario.js
+++ binario.js
@@ -1,3 +1,20 @@
-function crear_binario(num){
-    
+num = 75;
+
+function crear_binario(){
+    num =74;
+    while(num != 2){
+        h= num/2 
+
+     if(h%2 == 0 ){
+         exit1 = 0
+         return exit1 
+     }
+     else{
+         exit2 = 1 
+          exit2 
+     }
+    }
+     exit= exit1 + exit2
+     document.write (exit)
 }
+
