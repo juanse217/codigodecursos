8717799a5a0e0c98bda93a9cfe55548d6c5e17b8
sebas <juansebas08004@gmail.com>
2021-05-20 21:11:59 -0500

    primeros pasos javascript

---
+++ cursojavascript/variables.js
@@ -0,0 +1,20 @@
+var nombre = "Pepe";
+
+//declarar variables 
+
+var edad;
+
+edad
+//inicializar variable
+
+edad = 40;
+
+edad
+
+var elementos = ["pc", "celular"];
+
+var persona = {
+    nombre: "pepe",
+    edad: 30
+}
+persona; 
